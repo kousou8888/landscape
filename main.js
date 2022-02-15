4e0897f5c65c32ad220ff3e3ee47c64d0f2647a6
kousou1107 <kousou8888@gmail.com>
2022-02-15 23:24:18 +0900

    update 2022/02/15 23:24

--- main.js
+++ main.js
@@ -89,11 +89,14 @@ hoverEvent();
 
 
 // ▼ Slick設定
-$('.albums-tab').slick({
-    autoplay: true, //自動再生
-    dots: true, //ドットのナビゲーションを表示
-    infinite: true, //スライドのループ有効化
-    speed: 1000, //切り替えのスピード（小さくすると速くなる）
-    fade: true, //フェードの有効化
-    adaptiveHeight: true, //スライドの高さの自動調整
+$("document").ready(function () {
+    $('.albums-tab').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 1800,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '25%',
+        variableWidth: true,
+    });
 });
