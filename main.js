d3df46190db8676a7c2903d3307edd3201e269f7
kousou1107 <kousou8888@gmail.com>
2022-02-20 23:35:31 +0900

    update 2022/02/20 23:35

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ let activeImageIndex = 0;
 window.onload = function() {
     width = window.innerWidth;
     show(width);
+    background();
 }
 window.onresize = function() {
     width = window.innerWidth;
     show(width);
+    background();
 }
 function show(showWidth) {
     // console.log(showWidth);
@@ -39,11 +41,39 @@ function nextImage(){
 }
 
 
+// ▼　topロゴ移動コード
+const scrollAnimeTop = function () {
+    const classE = document.getElementById("latest");
+    const ElementSvg = document.getElementById("oita-svg");
+    window.addEventListener("scroll", function () {
+        const scrollValue = window.pageYOffset;  //スクロール量の取得
+        const scrollEle = classE;  // 要素の取得
+        const windowHeight = window.innerHeight;  // 画面の高さを取得
+        const value = windowHeight / 1.5;  // はみ出させる値（お好みで設定）
+        const scrollTop = scrollEle.getBoundingClientRect().top + scrollValue;  // 取得した要素のtop値の取得 + スクロール量
+        if (scrollValue > scrollTop - windowHeight + value) {
+            ElementSvg.style.top = "2vh";
+            ElementSvg.style.left = "6%";
+            ElementSvg.style.width = "10%";
+            ElementSvg.style.position = "fixed";
+        }else{
+            ElementSvg.style.top = "15vh"
+            ElementSvg.style.left = "50%"
+            ElementSvg.style.width = "30%"
+            ElementSvg.style.position = "absolute";
+        }
+    });
+}
+scrollAnimeTop();
+
+
+
 // ▼　スクロールアニメーション
 const scrollEvent = function () {
     const ClassElem = document.getElementsByClassName("scroll");  //下からフェードイン
-    const ClassElemRight = document.getElementsByClassName("scroll-right");  //右からフェードイン
-    const ClassElemLeft = document.getElementsByClassName("scroll-left");  //右からフェードイン
+    const ClassElemRight = document.getElementsByClassName("pic-cartain-left");  //右からフェードイン
+    const ClassElemLeft = document.getElementsByClassName("pic-cartain-right");  //右からフェードイン
+    const ClassBgWrap = document.getElementsByClassName("al-text");  //右からフェードイン
 
     const scrollAnime = function (classE) {
         window.addEventListener("scroll", function () {
@@ -56,7 +86,7 @@ const scrollEvent = function () {
                 if (scrollValue > scrollTop - windowHeight + value) {
                     scrollEle[a].classList.add("js-fade");
                 }else{
-                    scrollEle[a].classList.remove("js-fade");
+                    // scrollEle[a].classList.remove("js-fade");
                 }
             }
         });
@@ -64,6 +94,7 @@ const scrollEvent = function () {
     scrollAnime(ClassElem);
     scrollAnime(ClassElemRight);
     scrollAnime(ClassElemLeft);
+    scrollAnime(ClassBgWrap);
 };
 scrollEvent();
 
@@ -100,3 +131,22 @@ $("document").ready(function () {
         variableWidth: true,
     });
 });
+
+
+//  アルバムエリアの背景パララックス
+const background = function(){
+    $('.contents').each(function(i, elem){
+        var contentsPOS = $(elem).offset().top;
+        $(window).on('load scroll resize', function(){
+            var winHeight = $(window).height();
+            var scrollTop = $(window).scrollTop();
+            var showClass = 'show';
+            var timing = winHeight / 3; // 100pxコンテンツが見えたら次のif文がtrue
+            if (scrollTop >= contentsPOS - winHeight + timing){
+              $(elem).addClass(showClass);
+            } else {
+              $(elem).removeClass(showClass);
+            }
+        });
+    });
+}
