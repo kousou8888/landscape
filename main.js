aef3a959ec7731f2f347848e0d9b71b357ccd1e8
kousou1107 <kousou8888@gmail.com>
2022-02-23 12:40:04 +0900

    update 2022/02/23 12:39

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ const scrollEvent = function () {
             const scrollValue = window.pageYOffset;  //スクロール量の取得
             const scrollEle = classE;  // 要素の取得
             const windowHeight = window.innerHeight;  // 画面の高さを取得
-            const value = 200;  // はみ出させる値（お好みで設定）
+            const value = 100;  // はみ出させる値（お好みで設定）
             for (let a = 0; a < scrollEle.length; a++) {
                 const scrollTop = scrollEle[a].getBoundingClientRect().top + scrollValue;  // 取得した要素のtop値の取得 + スクロール量
                 if (scrollValue > scrollTop - windowHeight + value) {
