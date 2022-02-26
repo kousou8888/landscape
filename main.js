e69a1bf310e7a7858045926096dee855aae309aa
kousou1107 <kousou8888@gmail.com>
2022-02-26 20:40:44 +0900

    update 2022/02/26 20:40

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// topのスライドショー
 let width;
 let slider;
 let imageArray;
@@ -6,13 +5,33 @@ let activeImageIndex = 0;
 window.onload = function() {
     width = window.innerWidth;
     show(width);
-    background();
+    bgpara();
+    scrollEvent();
 }
 window.onresize = function() {
     width = window.innerWidth;
     show(width);
-    background();
+    bgpara();
+    scrollEvent();
 }
+
+
+const scrollEvent = function () {
+    const ClassElem = document.getElementsByClassName("scroll");  //下からフェードイン
+    const ClassElemRight = document.getElementsByClassName("scroll-album");  //右からフェードイン
+    const ClassAlTitle = document.getElementsByClassName("album-title");  //右からフェードイン
+    const Height = window.innerHeight / 4;
+    scrolleve(ClassElem, Height, 'js-fade');
+    scrolleve(ClassElemRight, Height, 'js-fade');
+    scrolleve(ClassAlTitle, Height, 'js-fade');
+};
+const bgpara = function () {
+    const Ele = document.getElementsByClassName('contents');
+    const Height = window.innerHeight / 8;
+    scrolleve(Ele, Height, 'show'); 
+};
+
+// ▼　topのスライドショー
 function show(showWidth) {
     // console.log(showWidth);
     if (showWidth <= 767) {  //レスポンシブ切り替えの値
@@ -40,64 +59,23 @@ function nextImage(){
     setTimeout('nextImage()', 4000);
 }
 
-
-// ▼　topロゴ移動コード
-const scrollAnimeTop = function () {
-    const classE = document.getElementById("latest");
-    const ElementSvg = document.getElementById("oita-svg");
+// ▼　スクロールアニメーション
+const scrolleve = function(classE, TopPass, clList) {
     window.addEventListener("scroll", function () {
         const scrollValue = window.pageYOffset;  //スクロール量の取得
         const scrollEle = classE;  // 要素の取得
         const windowHeight = window.innerHeight;  // 画面の高さを取得
-        const value = windowHeight / 1.5;  // はみ出させる値（お好みで設定）
-        const scrollTop = scrollEle.getBoundingClientRect().top + scrollValue;  // 取得した要素のtop値の取得 + スクロール量
-        if (scrollValue > scrollTop - windowHeight + value) {
-            ElementSvg.classList.add('js-logo');
-            // ElementSvg.style.top = "2vh";
-            // ElementSvg.style.left = "6%";
-            // ElementSvg.style.width = "10%";
-            // ElementSvg.style.position = "fixed";
-        }else{
-            ElementSvg.classList.remove('js-logo');
-            // ElementSvg.style.top = "15vh"
-            // ElementSvg.style.left = "50%"
-            // ElementSvg.style.width = "30%"
-            // ElementSvg.style.position = "absolute";
-        }
+        const value = TopPass;  // はみ出させる値（お好みで設定）
+        for (let a = 0; a < scrollEle.length; a++) {
+            const scrollTop = scrollEle[a].getBoundingClientRect().top + scrollValue;  // 取得した要素のtop値の取得 + スクロール量
+            if (scrollValue > scrollTop - windowHeight + value) {
+                scrollEle[a].classList.add(clList);
+            }else{
+                scrollEle[a].classList.remove(clList);
+            };
+        };
     });
-}
-scrollAnimeTop();
-
-
-
-// ▼　スクロールアニメーション
-const scrollEvent = function () {
-    const ClassElem = document.getElementsByClassName("scroll");  //下からフェードイン
-    const ClassElemRight = document.getElementsByClassName("scroll-album");  //右からフェードイン
-    const ClassAlTitle = document.getElementsByClassName("album-title");  //右からフェードイン
-
-    const scrollAnime = function (classE) {
-        window.addEventListener("scroll", function () {
-            const scrollValue = window.pageYOffset;  //スクロール量の取得
-            const scrollEle = classE;  // 要素の取得
-            const windowHeight = window.innerHeight;  // 画面の高さを取得
-            const value = 100;  // はみ出させる値（お好みで設定）
-            for (let a = 0; a < scrollEle.length; a++) {
-                const scrollTop = scrollEle[a].getBoundingClientRect().top + scrollValue;  // 取得した要素のtop値の取得 + スクロール量
-                if (scrollValue > scrollTop - windowHeight + value) {
-                    scrollEle[a].classList.add("js-fade");
-                }else{
-                    // scrollEle[a].classList.remove("js-fade");
-                }
-            }
-        });
-    }
-    scrollAnime(ClassElem);
-    scrollAnime(ClassElemRight);
-    scrollAnime(ClassAlTitle);
 };
-scrollEvent();
-
 
 // ▼ Slick設定
 $("document").ready(function () {
@@ -112,21 +90,21 @@ $("document").ready(function () {
     });
 });
 
-
-//  アルバムエリアの背景パララックス
-const background = function(){
-    $('.contents').each(function(i, elem){
-        var contentsPOS = $(elem).offset().top;
-        $(window).on('load scroll resize', function(){
-            var winHeight = $(window).height();
-            var scrollTop = $(window).scrollTop();
-            var showClass = 'show';
-            var timing = winHeight / 3; // コンテンツが見えたら次のif文がtrue
-            if (scrollTop >= contentsPOS - winHeight + timing){
-              $(elem).addClass(showClass);
-            } else {
-              $(elem).removeClass(showClass);
-            }
-        });
+// ▼　topロゴ移動コード
+const scrollAnimeTop = function () {
+    const classE = document.getElementById("latest");
+    const ElementSvg = document.getElementById("oita-svg");
+    window.addEventListener("scroll", function () {
+        const scrollValue = window.pageYOffset;  //スクロール量の取得
+        const scrollEle = classE;  // 要素の取得
+        const windowHeight = window.innerHeight;  // 画面の高さを取得
+        const value = windowHeight / 1.5;  // はみ出させる値（お好みで設定）
+        const scrollTop = scrollEle.getBoundingClientRect().top + scrollValue;  // 取得した要素のtop値の取得 + スクロール量
+        if (scrollValue > scrollTop - windowHeight + value) {
+            ElementSvg.classList.add('js-logo');
+        }else{
+            ElementSvg.classList.remove('js-logo');
+        }
     });
 }
+scrollAnimeTop();
